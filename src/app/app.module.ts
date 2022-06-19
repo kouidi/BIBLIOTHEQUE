cfe5e13261013f827459884ddb9ae7e22b4e23c1
_Hatim_ <kouidi.hatim@gmail.com>
2022-06-19 12:53:01 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './view/menu/menu.component';
+import { FooterComponent } from './view/footer/footer.component';
+import { ContentComponent } from './view/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
